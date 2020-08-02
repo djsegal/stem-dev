71ebd59f8edf9d17436db177220de8d266a7d24a
Dan Segal <dansegal2@gmail.com>
2020-08-01 20:46:56 -0400

    Add Water State interactive prototype

---
+++ Library/WaterStateAnimations/DATA/pages/page-1.js
@@ -0,0 +1,28 @@
+pageData[1] = {
+  "chapter": 1,
+  "unit": 1,
+  "pageTitle": "The phases of water",
+  "paragraph0": `The particles of matter are in constant random motion.
+  The phases of solid, liquid, and gas depend on the balance between the
+  intermolecular forces and the energy in random motion.
+  Intermolecular forces are attractive and tend to bring particles together.
+  Random motion is disruptive, and tends to push parciels apart.
+  The random motion increases with temperature. This is the source of thermal energy.`,
+  "paragraph1": `In a solid, intermolecular forces are stronger than .`,
+  "video1": "",
+  "gif1": "H2O_solid.gif",
+  "playtime1": 2,
+  "audio1": "",
+  "image1": "",
+  "paragraph2": `This is the 2nd paragraph.`,
+  "video2": "",
+  "gif2": "H2O_liquid.gif",
+  "playtime2": 2,
+  "image2": "",
+  "paragraph3": "",
+  "question": "Answer Yes",
+  "questionChoices": [
+    "Yes",
+    ["No", "Try again!"]
+  ]
+};
