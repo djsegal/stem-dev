8b130df4379bc978636eaa65c78f6fc5d3a7eca9
Dan Segal <dansegal2@gmail.com>
2020-08-01 20:45:59 -0400

    Add Lewis Structures interactive prototype

---
+++ Library/CHM_DrawingLewisStructures/DATA/pages/page-1.js
@@ -0,0 +1,45 @@
+pageData[1] = {
+  "chapter": 1,
+  "unit": 1,
+  "pageTitle": "Valence electrons",
+  "paragraph0": `Not all the electrons in an atom contribute to forming chemical bonds.<br>
+    Electrons in full energy levels do not form bonds.<br>
+    For example, magnesium has 12 electrons. The first 10 are in filled<br>
+    energy levels 1s, and 2s, 2p. These electrons do not form bonds.<br>
+    The remaining 2 electrons are in the partly filled third energy level.<br>
+    These electrons are the ones that form chemical bonds.<br><br>
+    These two electrons are the valence electrons.`,
+  "paragraph1": "<br>Divergent boundary<br><br>",
+  "video1": "DivergentBoundary.mp4",
+  "gif1": "",
+  "playtime1": 2,
+  "audio1": "",
+  "image1": "",
+  "paragraph2": "<br><br>Transform boundary.<br><br>",
+  "video2": "TransformBoundary.mp4",
+  "gif2": "",
+  "playtime2": 1,
+  "audio2": "",
+  "image2": "",
+  "paragraph3": "<br><br>Convergent Boundary<br><br>",
+  "video3": "ConvergentBoundary.mp4",
+  "gif3": "",
+  "playtime3": 1,
+  "audio3": "",
+  "image3": "",
+  "paragraph4": `<br><br>The Himalaya mountains lie between India and China.<br>
+  These mountains contain the highest elevations on Earth's surface<br>
+  including Mt. Everest which rises 8,848 m above sea level.<br>
+  The Himalays are still getting taller by 5 mm per year.<br><br>`,
+  "video4": "",
+  "gif4": "",
+  "playtime4": 1,
+  "audio4": "",
+  "image4": "Himalaya.png",
+  "question": "The Himalayan mountains are at a plate boundary. Which of the three is most likely?",
+  "questionChoices": [
+    "(a) a convergent boundary",
+    ["(b) a divergent boudary","No because divergent boundaries form rift valleys."],
+    ["(c) a transform boundary", "No because transform boundaries often cause earthquakes but do not tend to form large mountain ranges."]
+  ]
+};
